efac8f6ea2af1c546e5270763329ecf9ba2f6aee
MdMoonRahmanNayem <mmrn3182@gmail.com>
2025-12-06 10:09:46 +0600

    secure key

---
+++ src/firebase/firebase.config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "your_api_key_here",
+  authDomain: "your_auth_domain_here",
+  projectId: "your_project_id_here",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
